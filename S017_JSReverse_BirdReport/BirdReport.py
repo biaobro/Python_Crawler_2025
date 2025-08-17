994bf2e51e1facbbfd203439ac927a03815782a8
biaobro <biaobro@gmail.com>
2025-08-17 20:43:43 +0800

    add operation of insert data into db

--- S017_JSReverse_BirdReport/BirdReport.py
+++ S017_JSReverse_BirdReport/BirdReport.py
@@ -19,6 +19,17 @@ import execjs
 from loguru import logger
 import pandas as pd
 
+import pymysql  # 导入 MySQL 连接器模块
+
+conn = pymysql.connect(
+    host='localhost',  # 数据库主机地址
+    user='root',  # 数据库用户名
+    password='Wazg@2020',  # 数据库密码
+    database='runoob'  # 数据库名称
+)
+
+cursor = conn.cursor()  # 创建游标对象，用于执行 SQL 语句
+
 headers = {
     'Accept': 'application/json, text/javascript, */*; q=0.01',
     'Accept-Language': 'zh-CN,zh;q=0.9',
@@ -41,7 +52,7 @@ headers = {
 }
 
 df = pd.DataFrame()
-for page in range(1, 6):
+for page in range(1, 2):
     options = f'page={page}&limit=20'
     params = execjs.compile(open('app_whole.js', 'r', encoding='utf-8').read()).call('ajaxSetup', options)
 
@@ -56,6 +67,39 @@ for page in range(1, 6):
     # logger.debug(respDataDecrypt)
     # logger.debug(type(respDataDecrypt))
     logger.debug(f'page {page} get {len(respDataDecrypt)} records successfully.')
+
+    # 定义 SQL 插入语句
+    # 对于int 类型的字段，不需要使用 %d，统一使用 %s
+    sql = (
+        "INSERT INTO birdreport (address,endTime,id,outsideCount,pointId,pointName,reportId,serialId,startTime,"
+        "taxonCount,userId,username,visitsCount)"
+        "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
+
+    dataFrame = pd.DataFrame(respDataDecrypt)
+    data_list = list(zip(dataFrame['address'],
+                         dataFrame['endTime'],
+                         dataFrame['id'],
+                         dataFrame['outsideCount'],
+                         dataFrame['pointId'],
+                         dataFrame['pointName'],
+                         dataFrame['reportId'],
+                         dataFrame['serialId'],
+                         dataFrame['startTime'],
+                         dataFrame['taxonCount'],
+                         dataFrame['userId'],
+                         dataFrame['username'],
+                         dataFrame['visitsCount']))
+    # logger.debug(dataFrame['address'])
+    # logger.debug(data_list)
+    try:
+        cursor.executemany(sql, data_list)
+        conn.commit()
+        logger.debug("插入成功")
+    except Exception as e:
+        logger.debug("插入失败:", str(e))
+        conn.rollback()
+
     df = pd.concat([df, pd.DataFrame(respDataDecrypt)])
     time.sleep(10)
 df.to_excel(f'BirdReport_{datetime.datetime.now().strftime("%Y%m%d%H%M%S")}.xlsx', index=False)
+conn.close()
